6a1d284d25d39cc6dff07f8c5096bec2bb0f1b54
Raymond Klawitter <rayklawitter@gmail.com>
2024-03-12 08:53:58 -0400

    Updated README and added Documentation_Again ex1

---
+++ Documentation_Again/ex1.rb
@@ -0,0 +1,4 @@
+=begin
+  File::path is a CLASS method returns the string representation of the path given
+  File#path is and INSTANCE method returns the pathname used to create file as a string
+=end
