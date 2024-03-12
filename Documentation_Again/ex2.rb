6a9bab08d0324689b22191f7db5125b264b1005f
Raymond Klawitter <rayklawitter@gmail.com>
2024-03-12 16:09:04 -0400

    Added exercises 2 through 9 for Documentation Again

---
+++ Documentation_Again/ex2.rb
@@ -0,0 +1,10 @@
+=begin
+
+require 'date' - loads Date class which provides methods for sotring and manipulating calendar dates
+
+puts Date.civil               => -4717-01-01
+puts Date.civil(2016)         => 2016-01-01
+puts Date.civil(2016, 5)      => 2016-05-01
+puts Date.civil(2016, 5, 13)  => 2016-05-13
+
+=end
---
+++ Documentation_Again/ex9.rb
@@ -0,0 +1,4 @@
+=begin
+The verbiage under YAML metions "As the implementation is provided by the Psych library, detailed documentation can be found in that library’s docs"
+When we look under the Psych library we find the load_file method
+=end
